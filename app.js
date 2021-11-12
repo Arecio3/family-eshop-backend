c5d378fe77dd976ec3ad1d2a98afc49e6b59f45a
Arecio Canton <devmain@Arecios-iMac.fios-router.home>
2021-11-12 11:42:41 -0500

    Morgan logs and access.log file

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const app = express();
-
+const fs = require('fs');
+const path = require('path');
+const morgan = require('morgan');
 require('dotenv/config');
 const api = process.env.API_URL;
 
-// specify inital route
-app.get(api + '/', (req, res) => {
-    res.send('Hello API!');
+// ----- Middleware --- (function that has control of req, res of any API)
+// Parse JSON 
+app.use(express.json())
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+// Using Morgan for logging API requests
+app.use(morgan('tiny'))
+// Morgan writes a file for API logs
+app.use(morgan('combined', { stream: accessLogStream }))
+// ----- End of Middleware -----
+
+// get product route = http://localhost:3000/api/v1/products
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'hair-dresser',
+        image: "some_url"
+    }
+    res.send(product);
+})
+// specify inital route = http://localhost:3000/api/v1/products
+app.post(`${api}/products`, (req, res) => {
+    // grabs data from DOM body
+    const newProduct = req.body;
+    console.log(newProduct)
+    // Sends to frontend
+    res.send(newProduct);
 })
 
 // Run server with msg on success
