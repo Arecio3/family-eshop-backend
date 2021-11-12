42a3fbf6b7f3d7e0297463b1be48cf71f5dc914a
Arecio Canton <devmain@Arecios-iMac.fios-router.home>
2021-11-12 15:09:10 -0500

    Post Category Route

--- routers/categories.js
+++ routers/categories.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { Category } = require('../models/category');
 
+// Find Categories
 router.get(`/`, async (req, res) => {
     const categoryList = await Category.find();
 
@@ -10,5 +11,25 @@ router.get(`/`, async (req, res) => {
     }
     res.send(categoryList);
 })
+
+// Post Categories
+router.post('/', async (req, res) => {
+    // create new category 
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color,
+    })
+    // waits async to save
+    category = await category.save();
+
+    // if theres no category
+    if (!category)
+    return res.status(404).send('Category was not created');
+
+    // send to frontend
+    res.send(category);
+})
+
 
 module.exports = router;
