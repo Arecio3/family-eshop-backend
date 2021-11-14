99450ed0453820944cdef233a01060ad368c47e9
Arecio Canton <devmain@Arecios-iMac.fios-router.home>
2021-11-14 15:46:20 -0500

    Validating IMG upload

--- routers/products.js
+++ routers/products.js
@@ -3,6 +3,36 @@ const { Category } = require("../models/category");
 const router = express.Router();
 const { Product } = require("../models/product");
 const mongoose = require("mongoose");
+const multer = require('multer');
+
+// Valid File extensions
+const FILE_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpeg'
+}
+
+// Img Upload
+const storage = multer.diskStorage({
+  // Control over image file destination
+  destination: function (req, file, cb) {
+    const isValidFile = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error('Invalid IMG type');
+    if(isValidFile) {
+      uploadError = null
+    }
+    // If theres an error uploading, we assign dest
+    cb(uploadError, 'public/uploads')
+  },
+  // Creating Unique Filename
+  filename: function (req, file, cb) {
+    const fileName = file.originalname.replace(' ', '-');
+    const extension = FILE_TYPE_MAP[file.mimetype]
+    cb(null, `${fileName}-${Date.now()}.${extension}`)
+  }
+})
+
+const uploadOptions = multer({ storage: storage })
 
 // get all products route .select('name image -_id') populate is showing category details on JSON return
 router.get(`/`, async (req, res) => {
@@ -40,17 +70,23 @@ router.get(`/:id`, async (req, res) => {
 });
 
 // post product route
-router.post(`/`, async (req, res) => {
+router.post(`/`, uploadOptions.single('image'), async (req, res) => {
   // Check if category exist
   const category = await Category.findById(req.body.category);
   if (!category) return res.status(400).send("Invalid Category");
+
+  // Multer IMG
+  const fileName = req.file.filename
+
+  // URL Path to IMG
+  const basePath = `${req.protocol}://${req.get('host')}/public/upload/`;
 
   // Creating new product with request from body of DOM
   let product = new Product({
     name: req.body.name,
     description: req.body.description,
     richDescription: req.body.richDescription,
-    image: req.body.image,
+    image: `${basePath}${fileName}`,
     brand: req.body.brand,
     price: req.body.price,
     category: req.body.category,
